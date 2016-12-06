a8fb526a1f230d1258d016bd0be5046f7f5b24fc
Franz Irnberger <franz.irnberger@gmail.com>
2016-12-06 08:58:56 +0100

    WS-2016.3.1 <ratna@DESKTOP-FSA5TP0 Create markdown.xml, IntelliLang.xml, myngcam.js, print.xml, project.default.xml, diff.xml, Default copy.xml, AngularJS .xml, applicationLibraries.xml, file.template.settings.xml, ignore.xml, Jasmine.xml, find.xml, keymap.xml, js-testing.html, code.style.schemes.xml, Ang-Unit-controller-simple.js, Angular2.xml, Dart.xml, webstorm-angular-live-templates.xml, github_settings.xml, Dart surround.xml, abbreviations.xml, Default.xml, Franz Scheme.icls, JavaScript.xml, _myJasmine.xml, Ang-Dirctive.html, Zen XSL.xml, Handlebars.xml, editor.codeinsight.xml, editor.xml, webServers.xml, debugger.xml, baseRefactoring.xml, ide.general.xml, git.xml, Angular2-StyleGuide-LiveTemplates.xml, other.xml, ui.lnf.xml, filetypes.xml, xsl.xml, watcherDefaultTasks.xml, colors.scheme.xml, _myJavascript.xml, notifications.xml, _myAngular.xml, Ang-Controller.html, laf.xml, Meteor HTML.xml, path.macros.xml, ng2templates.xml, Default _1_.xml, web-browsers.xml, vcs.xml

---
+++ fileTemplates/myngcam.js
@@ -0,0 +1,22 @@
+angular.module('$app$')
+    .controller('$controllerName$', $controllerName$);
+
+/*
+ * 
+ *  
+ * controller: $controllerName$
+ */
+
+$controllerNameInject$.inject['$injectable$'];
+
+function $controllerName$($injectable$) {
+    var vm = this;
+    vm.title = '$controllerName$';
+
+    activate();
+
+    function activate() {
+        
+    }
+
+}
